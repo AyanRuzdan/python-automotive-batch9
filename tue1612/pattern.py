34b567628ddcfe2dd33dc7987f9c6b2deb0047f7
Ayan Ruzdan <ayanruzdan@gmail.com>
2025-12-16 16:23:37 +0530

    Refactor recap notebook: Add string formatting section and enhance history explanation with additional details.

--- tue1612/pattern.py
+++ tue1612/pattern.py
@@ -1,6 +1,38 @@
+"""Write a program that takes an integer n as input
+and prints a right-angled triangle star pattern of height
+n using nested loop
+"""
+# take integer input
 n = int(input("Enter triangle height: "))
-if n > 0:
-    for i in range(1, n+1):
-        for j in range(i):
-            print("*", end="")
-        print()
+
+# define function to print the triangle using n as argument
+
+
+def print_nested(n):
+    print("Using nested loop")
+    # validate input
+    if n > 0:
+
+        # outer loop to initialize the rows
+        for i in range(n):
+
+            # inner loop to print the star for each row
+            for _ in range(i+1):
+
+                # end prevents print function to go to newline
+                print("*", end="")
+
+            # empty print function only prints newline
+            print()
+
+
+# call the printing functions
+print_nested(n)
+
+print("\nUsing list comprehension")
+
+""" using n as range going from 0 to n (n+1 excluded)
+    call the print function inside the list
+    use string multiplier instead of for loop
+    """
+[print("*"*i) for i in range(n+1)]
