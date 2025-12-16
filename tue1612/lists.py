9b69cc815348c9bcce595f4a8b4d16f63069edd1
Ayan Ruzdan <ayanruzdan@gmail.com>
2025-12-16 12:12:33 +0530

    Format list operations for improved readability and consistency

--- tue1612/lists.py
+++ tue1612/lists.py
@@ -1,18 +1,32 @@
 # list operations and outputs
 mylist = [14, 1, 3, 1, 5, 7, 9]
 print("Mylist\n", mylist)
+
+# append to end of list
 mylist.append(-99)
 print("\nAppend -99\n", mylist)
+
+# remove first occurrence
 mylist.remove(1)
 print("\nRemoved first occurence of 1\n", mylist)
+
+# pop from end of list
 mylist.pop()
 print("\nPopped last element\n", mylist)
+
+# pop index
 mylist.pop(2)
 print("\nPopped index 2\n", mylist)
+
+# insert second value at given index
 mylist.insert(3, 144)
 print("\nInserted 144 at index 3\n", mylist)
+
+# sort the list, do not reassign
 mylist.sort()
 print("\nSorted list\n", mylist)
+
+# clear all elements and get empty list
 mylist.clear()
 print("\nList cleared\n", mylist)
 
@@ -26,5 +40,5 @@ nums.extend([6, 7, 8, 9])
 print(nums)
 even_cubes = [x**3 for x in nums if x % 2 == 0]
 print("\nEven cubes:", even_cubes)
-keep_all = [x if x%3 else -1 for x in nums]
+keep_all = [x if x % 3 else -1 for x in nums]
 print(keep_all)
