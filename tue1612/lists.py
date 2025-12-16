76c3430cb66412109d807cbbd18520fa7859a560
Ayan Ruzdan <ayanruzdan@gmail.com>
2025-12-16 11:45:19 +0530

    Refactor print statements for list operations and add sample dictionary with access methods

--- tue1612/lists.py
+++ tue1612/lists.py
@@ -2,27 +2,29 @@
 mylist = [14, 1, 3, 1, 5, 7, 9]
 print("Mylist\n", mylist)
 mylist.append(-99)
-print("Append -99\n", mylist)
+print("\nAppend -99\n", mylist)
 mylist.remove(1)
-print("Removed first occurence of 1\n", mylist)
+print("\nRemoved first occurence of 1\n", mylist)
 mylist.pop()
-print("Popped last element\n", mylist)
+print("\nPopped last element\n", mylist)
 mylist.pop(2)
-print("Popped index 2\n", mylist)
+print("\nPopped index 2\n", mylist)
 mylist.insert(3, 144)
-print("Inserted 144 at index 3\n", mylist)
+print("\nInserted 144 at index 3\n", mylist)
 mylist.sort()
-print("Sorted list\n", mylist)
+print("\nSorted list\n", mylist)
 mylist.clear()
-print("List cleared\n", mylist)
+print("\nList cleared\n", mylist)
 
 # list comprehension using two lists
 nums = [1, 2, 3, 4]
 cubes = [x**3 for x in nums]
-print("Cubes:", cubes)
+print("\nCubes:", cubes)
 
 # list comprehension using conditionals
 nums.extend([6, 7, 8, 9])
 print(nums)
 even_cubes = [x**3 for x in nums if x % 2 == 0]
-print("Even cubes:", even_cubes)
+print("\nEven cubes:", even_cubes)
+keep_all = [x if x%3 else -1 for x in nums]
+print(keep_all)
